b6ba3ed073ba0dd8d4da4e1aceeffc3cd0665857
huangyongxiang <huangyongxiang@baidu.com>
2018-03-09 00:16:10 +0800

    everythin is done

--- logistic_regression.py
+++ logistic_regression.py
@@ -25,86 +25,102 @@ DEBUG_MODE = True
 
 #params
 epoch = 1000
-#lr = 0.002 # learning rate
-lr = 40e-5 #learning rate
-learning_rate = {"breast-cancer":2e-3,"wine":50e-9, "digit":1e-3,"diabetes":16e-4, "iris":40e-5 }
+learning_rate = {"breast-cancer":2e-3,"wine":4e-7, "digit":1e-5,"diabetes":1e-3, "iris":40e-5 }
 
-def debug_log( info ):
-    if(DEBUG_MODE):
-        print("DEBUG:" ,info)
+VALID_INTERVAL = 10
 
-class logistic_regression(object):
+
+class LogReg(object):
     def __init__(self, dataset):
-        #self.dataset = dataset
-        #print(self.dataset)
         npzfile = np.load(dataset)
         self.trainX, self.trainY= npzfile['train_X'], npzfile['train_Y']
         self.testX, self.testY= npzfile['test_X'], npzfile['test_Y']
-        print("Finish loading " + dataset)
 
-        #initial weights with small random value in (-0.01, 0.01)
-        #self.W = (np.random.random(self.testX.shape[1]) - 0.5)/50.0
-        #self.w0 = (np.random.random(1) -0.5)/50.0
-        self.W = np.random.random(self.testX.shape[1])
-        self.w0 = np.random.random()
+        self.dataset_name = dataset.split('/')[-1].split(".")[0]
+        self.lr = learning_rate[self.dataset_name]
 
-        dataset_name = dataset.split('/')[-1].split(".")[0]
-        self.lr = learning_rate[dataset_name]
+        print("\nDataset: {}".format( self.dataset_name) )
 
     def sigmoid(self, x):
         return 1.0/(1.0+np.exp(-x))
 
-    def hypothesis_function(self, X, W, b):
-        return self.sigmoid(X @ W.T + b)
-
-    def classifier(self):
-        from sklearn.linear_model import SGDClassifier
-        clf = SGDClassifier(loss='log', penalty='none', n_iter=epoch, shuffle=False, verbose=True, learning_rate = 'constant', eta0 = self.lr)
-        clf.fit(self.trainX, self.trainY)
-        predY = clf.predict(self.testX)
-        acc=accuracy_score(self.testY, predY)
-        print("test accurarcy: ", acc)
+    def lr_func(self, X, W, b):
+        z = W @ X.T + b
+        return self.sigmoid(z)
 
     def train(self):
-        #parameters
-        X = self.trainX
-        Y = self.trainY
-        testX = self.testX
-        testY = self.testY
+        self.loss_record=[]
+        self.acc_record=[]
 
-        loss = np.empty(epoch)
-        test_loss = np.empty(epoch)
+        #initial weights with small random value in (-0.01, 0.01)
+        self.W = (np.random.random(self.testX.shape[1]) - 0.5)/50.0
+        self.w0 = (np.random.random(1) -0.5)/50.0
+
         start_time = datetime.now()
         for i in range(0, epoch):
-            #calc hythoposis values, i.e. predictions
-            #theta = X @ self.W.T + self.w0
-            #H = self.sigmoid(theta).flatten() # r
-            H = self.hypothesis_function(X, self.W, self.w0)
-
-            #calc cross-entropy loss, i.e. log loss
-            loss[i] = log_loss(Y, H)
-            testH= self.hypothesis_function(testX, self.W, self.w0)
-            test_loss[i] = log_loss(testY, testH)
-            debug_log("{}\t{}".format(loss[i], test_loss[i]))
-
             #batch gradient descent
-            err = Y-H
-            self.W += self.lr * np.sum((err) @ X)
-            self.w0 += self.lr * np.sum(err)
-        time_used =  datetime.now() - start_time
-        print("Finish training for {} epoch in {}".format(epoch, time_used))
-        print("Final traing loss: ", loss[epoch-1])
-        print("Final test loss: ", test_loss[epoch-1])
+            prob = self.lr_func(self.trainX, self.W, self.w0)
+            e = self.trainY - prob
+            self.W += (e @ self.trainX) * self.lr
+            self.w0 += np.sum(e, 0) * self.lr
 
-    def evaluate(self):
-        score = self.hypothesis_function(self.testX, self.W, self.w0)
-        predY = (score > 0.5) * 1
-        acc=accuracy_score(self.testY, predY)
-        print("Classification Accuracy on test set: ", acc)
-    # plot loss and test_loss here, report accuracy here
-    def plot_loss(train_loss, test_loss):
-        pass
-    #def validation(self):
+            #watch and record performance change over time
+            if( (i+1) % VALID_INTERVAL== 0):
+                '''
+                loss_train = log_loss(self.trainY, prob)
+                pred = (prob > 0.5) * 1
+                acc_train = accuracy_score(self.trainY, pred)
+                '''
+                # evaluate performance on test set
+                loss_test ,acc_test = self.validate()
+                self.loss_record.append(loss_test)
+                self.acc_record.append(acc_test)
+
+        # evaluate everything needed here
+        loss_train = log_loss(self.trainY, prob)
+        acc_train = accuracy_score(self.trainY, (prob > 0.5) * 1)
+
+        loss_test ,acc_test = self.validate()
+
+        time = datetime.now() - start_time
+        print("Finish training 1000 epoches in ", time)
+        print("=> Loss in training set: {:.4f}".format(loss_train))
+        print("=> Accuracy in training set: {:.4f}".format(acc_train))
+        print("=> Loss in test set: {:.4f}".format(loss_test))
+        print("=> Accuracy in test set: {:.4f}".format(acc_test))
+
+        #plot the performance changing curve
+        #self.plot_curve(self.loss_record
+
+        #return self.acc_record
+
+    def validate(self):
+        #use Z score or prob as decision function
+        prob = self.lr_func(self.testX, self.W, self.w0)
+        pred = (prob > 0.5) * 1
+        loss = log_loss(self.testY, prob)
+        acc = accuracy_score(self.testY, pred)
+        #print("Loss on test set: ", loss)
+        #print("Classification Accuracy on test set: ", acc)
+
+        return loss,acc
+
+def plot_curve( records):
+    # plot with various axes scales
+    plt.figure("Performance (test accurarcy) change over time during training")
+    x = list(range(0,1000,VALID_INTERVAL))
+
+    pos=231
+    for dn in records:
+        plt.subplot(pos)
+        plt.title(dn)
+        plt.plot(x, records[dn])
+        plt.xlabel("Epoch")
+        plt.ylabel("Acc")
+        plt.axis([0, 1000, 0, 1])
+        plt.grid(True)
+        pos+=1
+    plt.show()
 
 if __name__=="__main__":
     # interpret args
@@ -115,14 +131,14 @@ if __name__=="__main__":
     dataset = args.dataset
     #epoch = args.epoch
 
-    if(dataset!=None):
-        LR = logistic_regression(args.dataset)
+    datasets = glob.glob( '../datasets' + '/*.npz')
+    records ={}
+    for dataset in datasets:
+        LR = LogReg(dataset)
         LR.train()
-        LR.evaluate()
 
-        #LR.classifier()
-    else:
-        datasets = glob.glob( '../datasets' + '/*.npz')
-        for dataset in datasets:
-            LR = logistic_regression(dataset)
-            LR.train()
+        records[LR.dataset_name] = LR.acc_record
+
+    plot_curve(records)
+
+
